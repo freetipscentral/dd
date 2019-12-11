8ddf328b201a984117748c4b7a335844550dccda
Nilendu Bhattacharya <nilendu.bhattacharya@gmail.com>
2019-12-10 20:50:35 -0600

    More Integration

--- src/components/QuizOptions/QuizOptions.js
+++ src/components/QuizOptions/QuizOptions.js
@@ -1,21 +1,50 @@
 import React, {Component} from 'react';
 import QuizOption from './QuizOption';
-import styles from './QuizOptions.module.css'
+import styles from './QuizOptions.module.css';
 
 class QuizOptions extends Component {
-    render() {         
-        if(this.props.quizList) {            
-            const quizList = this.props.quizList.map((eachQuiz, i) => {
+    
+    constructor(props) {
+        super(props);
+        this.state = { 
+            quizList: this.props.quizList,
+            selectedQuizId: this.props.quizList[0].id
+        };
+    }
+
+    rightAnswerChangeHandler = (e) => {
+        this.setState({selectedQuizId: e.target.value})
+    }
+
+    submitQuizDetails = (e) => {
+        e.preventDefault();
+        this.props.onSelect(this.state.selectedQuizId);
+    }
+
+    render() {       
+        if(this.state.quizList) {
+            const quizzes = this.state.quizList.map((eachQuiz, i) => {
                 return (
-                    <QuizOption
-                        id={eachQuiz.id}
-                        quizName={eachQuiz.quizName}
-                        quizDetails={eachQuiz.quizDetails}                    
-                    />
+                    <option value={eachQuiz.id}>{eachQuiz.quizName}</option>
                 )
             });
-            return (   
-                <div className={styles.QuizOptions}>{quizList}</div>
+
+            return (                
+                <div className={styles.QuizOptions}>
+                    <form onSubmit={this.submitQuizDetails}>
+                        <div>
+                            <label>Available Quizzes:</label>
+                        </div>
+                        <div> 
+                            <select value={this.state.selectedQuiz} 
+                                onChange={e => this.rightAnswerChangeHandler(e)}>{quizzes}
+                            </select>
+                        </div>
+                        <div>                        
+                            <input type="submit" value="Select Quiz"/>
+                        </div>
+                    </form>
+                </div>                
             )
         }
     }
