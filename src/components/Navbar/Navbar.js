c7bd7e1ce48881b589fb74637f92ed7bc0a19f19
Nilendu Bhattacharya <nilendu.bhattacharya@gmail.com>
2019-12-29 23:07:02 -0600

    Added Links for routing

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import {Link} from 'react-router-dom'
 
 const navbar = props => (
     <Navbar style={{backgroundColor: 'Orange'}} variant="dark">
-        <Navbar.Brand href="/">AWS Quiz Time</Navbar.Brand>
+        <Navbar.Brand><Link to="/" style={{color:'White'}}>AWS Quiz Time</Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
-                <Nav.Link href="/aboutUs">About Us</Nav.Link>
-                <Nav.Link href="/faq">FAQ</Nav.Link>
+                <Link to="/aboutUs" style={{color:'White'}}>About Us</Link>
+                <Link to="/faq" style={{color:'White', marginLeft:'10px'}}>FAQ</Link>
             </Nav>
-        </Navbar.Collapse>
+        </Navbar.Collapse> 
     </Navbar>
 );
 
