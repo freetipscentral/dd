6062556ee23162ff6026ca13bb229e7de7cf7e02
Nilendu Bhattacharya <nilendu.bhattacharya@gmail.com>
2019-12-30 23:40:52 -0600

    Change Quiz Card Header

--- src/components/Homepage/DisplayQuizCard.js
+++ src/components/Homepage/DisplayQuizCard.js
@@ -5,15 +5,17 @@ import Button from 'react-bootstrap/Button';
 
 const displayQuizCard = props => {
     const borderStyle = { borderColor:'Orange', borderWidth: '.2rem' };
+    const cardTitleStyle = {backgroundColor: '#4a154b', color:'Orange', textAlign: 'center' };
+    
     return (
         <Col sm={12} lg={4} md={6}>
             <Card lg={4} sm={12} style={borderStyle}>
+                <Card.Header style = {cardTitleStyle}>{props.quizName}</Card.Header>
                 <Card.Body>
-                <Card.Title>{props.quizName}</Card.Title>
-                <Card.Text>
-                    {props.quizDetails}
-                </Card.Text>
-                <Button variant="primary">Start Quiz</Button>
+                    <Card.Text>
+                        {props.quizDetails}
+                    </Card.Text>
+                    <Button variant="primary">Start Quiz</Button>
                 </Card.Body>
                 <Card.Footer>
                     <small className="text-muted">Last updated 3 mins ago</small>
