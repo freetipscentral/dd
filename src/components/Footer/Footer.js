1d82dcfffcdea378455e4a015b4453d23164777d
Nilendu Bhattacharya <jsbynb@nilendub.com>
2020-01-27 21:42:32 -0600

    Fix footer on bottom

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,10 @@ import Navbar from 'react-bootstrap/Navbar';
 
 const footer = props =>  (
     <footer style={{marginTop: '20px'}}>
-        <Navbar bg="primary" variant="dark" fixed='botton'>
+        <Navbar bg="primary" variant="dark" fixed='bottom'>
             <Navbar.Brand href="#/">AWS Quiz Time</Navbar.Brand>
             <Navbar.Text>
-                Copyright 2019-2020: Nilendu Bhattacharya
+                Copyright 2019-2021: Nilendu Bhattacharya
             </Navbar.Text>
         </Navbar>
     </footer>
